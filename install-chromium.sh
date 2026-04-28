7305f2c0180b746f0bd4136491c2cad7612d3788
manivoosoghi-w <manivoosoghi@gmail.com>
2026-04-28 15:41:19 +0100

    Update install-chromium.sh

--- install-chromium.sh
+++ install-chromium.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Downloading Chromium..."
+echo "Downloading Chromium (stable build)..."
+
 mkdir -p chromium
-curl -Lo chromium/chrome.zip https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1250/chrome-linux.zip
+
+curl -Lo chromium/chrome.zip \
+https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1134310/chrome-linux.zip
 
 echo "Unzipping..."
 unzip chromium/chrome.zip -d chromium/
 
-echo "Done."
+echo "Chromium installed successfully."
